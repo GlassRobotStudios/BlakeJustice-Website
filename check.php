9f25ea46e79791a95b4b6595be756408e4d1e7fb
Adam Drakeford <adamdrakeford@gmail.com>
2013-04-18 15:56:33 +0100

    A little more security for the check.php page

--- check.php
+++ check.php
@@ -1,73 +1,56 @@
 <?php
-	// require_once('recaptchalib.php');
-
-	// $privatekey = "6LcP7N8SAAAAALcPPH8o-WRJzIVDpKA9Ns4G8wAL";
-	// $recaptchaChallenge = $_POST["modal-recaptcha_challenge_field"];
-	// $recaptchaResponse = $_POST["modal-recaptcha_response_field"];
-
-	// echo $recaptchaResponse . "\n";
-	// echo $recaptchaChallenge . "\n";
-
-	// $resp = recaptcha_check_answer ($privatekey, $_SERVER["REMOTE_ADDR"], $recaptchaChallenge, $recaptchaResponse);
-
-	// if (!$resp->is_valid) 
-	// {
-	// 	die ("The reCAPTCHA wasn't entered correctly. Go back and try it again."."(reCAPTCHA said: " . $resp->error . ")");
-	// } 
-	// else 
-	// {
-	// 	echo "OK";
-	// }
-
-	// ini_set("display_errors",1); 
-	// error_reporting(E_ALL);
-
-	// set_include_path(__dir__.'/dependencies');
-
 	require_once ('SendEmail.php');
 	
 	$name = $_POST["modal-inputName"];
 	$email = $_POST["modal-inputEmail"];
 	$message = $_POST["modal-inputMessage"];
 
-	$check = false;
+    $page = $_SERVER['HTTP_REFERER'];
+    $server = "http://".$_SERVER['SERVER_NAME']."/support";
 
-	if (filter_var($email, FILTER_VALIDATE_EMAIL)) 
-	{
-		$check = true;
-    }
-    else
+    if ($page == $server) 
     {
-    	die("Not a valid email or name");	
-    }
+    	$check = false;
 
-    if (!empty($name)) 
-    {
-    	$check = true;
-    }
-    else
-    {
-    	die("Not a valid email or name");
-    }
-
-    if ($check) 
-    {
-    	// echo "Valid";
-    	$var = SendEmail::sendAutomaticEmailTo($name, $email, $message);
-
-    	if ($var == 0) 
+    	if (filter_var($email, FILTER_VALIDATE_EMAIL)) 
     	{
-	    	header("Location: support?check=true");
-    	}
-    	else
-    	{
-    		header("Location: support?check=false&error=$var");	
-    	}
+    		$check = true;
+        }
+        else
+        {
+        	die("Not a valid email or name");	
+        }
+
+        if (!empty($name)) 
+        {
+        	$check = true;
+        }
+        else
+        {
+        	die("Not a valid email or name");
+        }
+
+        if ($check) 
+        {
+        	// echo "Valid";
+        	$var = SendEmail::sendAutomaticEmailTo($name, $email, $message);
+
+        	if ($var == 0) 
+        	{
+    	    	header("Location: support?check=true");
+        	}
+        	else
+        	{
+        		header("Location: support?check=false&error=$var");	
+        	}
+        }
+        else
+        {
+        	header("Location: support?check=false");
+        }
     }
     else
     {
-    	header("Location: support?check=false");
+        echo "You need to enter in the form on the support page";
     }
-
-
  ?>
